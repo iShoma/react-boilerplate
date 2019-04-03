a9c36f79a2723f002ba05f7eed60b7ed695a4a93
Махмутов Шамиль <s.mahmutov@corp.mail.ru>
2019-04-04 01:16:16 +0300

    delete build files

--- dist/homepage.5525cb3bed9cb2b5794a.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{40:function(n,e,t){"use strict";t.r(e);var u=t(0),a=t.n(u);e.default=function(){return a.a.createElement("div",null,"12312312")}}}]);
